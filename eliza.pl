58fad02b51fe2f6f7dd0ef1f6d77a37dcd03b392
Kai Trott <k.trott@web.de>
2014-12-10 15:29:35 +0100

    multiple answers, same questions

--- eliza.pl
+++ eliza.pl
@@ -16,18 +16,25 @@ read_sentence(Input) :-
 %   - see underscore('_') and numbers 0-9 as part of words
 %   - make lowercase
 
-eliza :- read_sentence(Input), eliza(Input), !.
-eliza([X]) :- poweroff(X), reply(['Goodbye. I hope I have helped you.']).
+
+random_response(R, RL) :- length(RL, Len), X is random(Len), nth0(X, RL, R).
+
+
+eliza :- read_sentence(Input), eliza_main(Input), !.
+
+eliza_main([X]) :- poweroff(X), reply(['Goodbye. I hope I have helped you.']).
 %TODO mehr schluss woerter
 %verschiedene Arten der Antwort, die aber das selbe aussagen --> mehr abwechslung
-eliza(Input) :-
-    pattern(Stimulus, Response),
+%Easter eggs -> i like trains --> Tschu, Tschu!
+eliza_main(Input) :-
+    pattern(Stimulus, ResponseList),
     match(Stimulus, Dictionary, Input),
-    match(Response, Dictionary, Output),
     !,
+    random_response(Response, ResponseList),
+    match(Response, Dictionary, Output),
     reply(Output),
     read_sentence(Input1),
-    eliza(Input1).
+    eliza_main(Input1).
 
 match([N|Pattern], Dictionary, Target) :-
     integer(N), lookup(N, Dictionary, LeftTarget),
@@ -46,17 +53,19 @@ add_memory(X, Predicate, Y) :-
     Fact =.. [Predicate, X, Y],
     asserta(Fact).
 
-pattern([i, am, 1], ['How', long, have, you, been, 1, '?']) :-
+pattern([i, am, 1], [['How', long, have, you, been, 1, '?'],
+                     ['Test', satz, mit, 1, '!']]) :-
     add_memory(i, am, X).
-pattern([1, you, 2, me], ['What', makes, you, think, 'I', 2, you, '?']).
-pattern([i, like, 1], ['Does', anyone, else, in, your, family, like, 1, '?']).
-pattern([i, feel, 1], ['Do', you, often, feel, that, way, '?']).
-pattern([1, X, 2], ['Please', tell, me, more, about, X, '.']) :- important(X).
+  % String wenn Wort Groß gescrieben wird
+pattern([1, you, 2, me], [['What', makes, you, think, 'I', 2, you, '?']]).
+pattern([i, like, 1], [['Does', anyone, else, in, your, family, like, 1, '?']]).
+pattern([i, feel, 1], [['Do', you, often, feel, that, way, '?']]).
+pattern([1, X, 2], [['Please', tell, me, more, about, X, '.']]) :- important(X).
 %pattern([remember, X, Predicate, Y], ['I', will, remember, that, '.']) :-
 % add_memory(X, Predicate, Y).
 %pattern([X, Predicate], ['Is', that, because, X, Predicate, Y, '?']) :-
 % call(Predicate, X, Y).
-pattern([1], ['Please', go, on, '.']).
+pattern([1], [['Please', go, on, '.'], ['weiter', 'bitte']]).
 
 important(father).
 important(mother).
